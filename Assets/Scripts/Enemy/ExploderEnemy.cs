df727a552e47b35934df3485ffc34d8146cb9aa8
GVictory1351989 <gvictorygamesin@gmail.com>
2025-07-03 18:00:53 +0530

    Changes in naming

--- Assets/Scripts/Enemy/ExploderEnemy.cs
+++ Assets/Scripts/Enemy/ExploderEnemy.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ExploderEnemy : Enemy
 {
+    /// <summary>
+    /// Move towards target different type of enemies one exploder enemy 
+    /// </summary>
+    /// <param name="targetPoint"></param>
     public override void MoveTowardsTarget(Vector3 targetPoint)
     {
         Vector3 direction = (targetPoint - transform.position).normalized;
